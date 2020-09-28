a4e882448619203a6f0364afb3ef83c1af5c5953
omerdemirkan <omerfarukpiano@gmail.com>
2020-09-27 18:13:56 -0700

    add puppeteer screenshot spwa funcitonality (load before evaluation)

--- src/puppeteer/index.ts
+++ src/puppeteer/index.ts
@@ -1,3 +1,14 @@
-import server from "./server";
+import puppeteer from "puppeteer";
 
-server.listen(5000, () => console.log("server listening on port 5000"));
+const spwaUrl = "https://synonymy-app.herokuapp.com/";
+
+(async function () {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto(spwaUrl, { waitUntil: "networkidle0" });
+
+    await page.screenshot({ path: "screenshot.png" });
+
+    await browser.close();
+})();
